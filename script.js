af449c08f7bd8001cae5e353efda5d1c6ba79850
WTHwithMyCodes <kertomulyo01@gmail.com>
2023-06-24 15:08:43 +0700

    vid1

---
+++ script.js
@@ -0,0 +1,63 @@
+// cara membuat Object pada JavaScript
+// 1. Object Literal
+
+// let mahasiswa1 = {
+//     nama: 'Sandhika',
+//     energi: 10,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`)
+//     }
+// }
+
+// let mahasiswa2 = {
+//     nama: 'Doddy',
+//     energi: 20,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`)
+//     }
+// }
+
+
+// 2. Function Declaration
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     mahasiswa.makan = function(porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan!`);
+//     }
+//     mahasiswa.main = function(jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain!`);
+//     }
+
+//     return mahasiswa;
+// }
+
+// let sandhika = Mahasiswa('Sandhika', 10);
+// let doddy = Mahasiswa('Doddy', 20);
+
+// 3. Constructor Function
+// keyword new
+function Mahasiswa(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+
+    this.makan = function(porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan!`);
+    }
+    this.main = function(jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat bermain!`);
+    }
+}
+
+let sandhika = new Mahasiswa('Sandhika', 10);
+
+
+// 4. Object.create
